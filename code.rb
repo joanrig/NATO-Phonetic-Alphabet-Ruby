e932a0c00e6995f61ed47333f747fe894c320b35
Joan Indiana Lyness <45588032+joanrig@users.noreply.github.com>
2019-02-23 17:35:52 -0500

    v3

--- code.rb
+++ code.rb
@@ -1,5 +1,5 @@
 #from CodeWars/ Kata https://www.codewars.com/kata/nato-phonetic-alphabet-ruby/train/ruby
-#Error at this point: Test Results: Expected: "One Two Tree", instead got: []
+
 
 
 
@@ -12,15 +12,13 @@ def nato(message)#=> 123abc??
   nato_letters = ALPHABET[0..25]
   nato_numbers = ALPHABET[26..36]
     
-  array.each_with_index do |char|
-    
-    if char.match(/\d/)
+  array.each do |char|
+    if (char.match(/\d/))
       answer<< nato_numbers[char]
-    
-    elsif nato_letters.include?(char)
-      answer<< nato_letters.find {|word| word[0] = char.upcase}
+    else
+      answer<< nato_letters.find {|word| word[0] == char.upcase}
     end
-  end      
+  end    
   answer
 end
             
