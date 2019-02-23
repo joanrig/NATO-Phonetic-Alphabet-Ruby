bc9c2904a38548e08f12c750a48e1db65687a8bd
Joan Indiana Lyness <45588032+joanrig@users.noreply.github.com>
2019-02-23 17:35:19 -0500

    v2

--- code.rb
+++ code.rb
@@ -1,29 +1,26 @@
 #from CodeWars/ Kata https://www.codewars.com/kata/nato-phonetic-alphabet-ruby/train/ruby
-#Error at this point: Test Results:
-#Expected: "One Two Tree", instead got: ["123"]
+#Error at this point: Test Results: Expected: "One Two Tree", instead got: []
+
 
 
 ALPHABET = %w(Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliet Kilo Lima Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey XRay Yankee Zulu Zero One Two Tree Four Fife Six Seven Eight Niner)
 
 def nato(message)#=> 123abc??
-  array = message.split(" ")#=> [1, 2, 3, "a", "b", "c"]
-  array.keep_if {|el| el =~ /[a-zA-Z0-9]/}
+  array = message.chars#=> [1, 2, 3, "a", "b", "c"]
+  answer = []
+  
+  nato_letters = ALPHABET[0..25]
+  nato_numbers = ALPHABET[26..36]
     
-  num_words = ALPHABET.pop(10) 
-  #%w(Zero One Two Tree Four Fife Six Seven Eight Niner)
-  #array = [1, 2, 3, "a", "b", "c"]
-  array.each_with_index do |el, index| 
-    if el.to_i =~ /[0-9]/
-      el = num_words[el] #ie 3 = num_words[3] = "Tree"
+  array.each_with_index do |char|
+    
+    if char.match(/\d/)
+      answer<< nato_numbers[char]
+    
+    elsif nato_letters.include?(char)
+      answer<< nato_letters.find {|word| word[0] = char.upcase}
     end
-    array
-  end
+  end      
+  answer
 end
             
-#   ALPHABET.each do |word|
-#     array.each do |el|
-#       el = word if word[0] == el.upcase
-#     end
-#   end
-#   array
-# end
